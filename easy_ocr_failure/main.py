6363b16bd893a4faf5fc2d096471fb9915c997c9
MinhKhoiNguyen <70082612+squidkidmax@users.noreply.github.com>
2026-07-31 22:23:34 +0700

    Delete easy_ocr_failure directory

--- easy_ocr_failure/main.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['vi'])  # 'vi' = Vietnamese
-results = reader.readtext('handwriting_image.jpg')
-
-for bbox, text, confidence in results:
-    print(f"{text} (confidence: {confidence:.2f})")
